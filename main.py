f016e301481579910fe38fa094fdbce4636b357c
UmrazKhan24 <umrazkhan007@gmail.com>
2025-02-02 17:59:01 +0530

    Adding merge sort code ti main.py file

--- main.py
+++ main.py
@@ -15,3 +15,32 @@ def quick_sort(arr):
         return quick_sort(lesser) + [pivot] + quick_sort(greater)
     
 print(quick_sort([5, 3, 7, 2, 8, 4, 1, 9, 6]))
+
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    else:
+        mid = len(arr) // 2
+        left = merge_sort(arr[:mid])
+        right = merge_sort(arr[mid:])
+        return merge(left, right)
+    
+def merge(left, right):
+    result = []
+    left_index = 0
+    right_index = 0
+    while left_index < len(left) and right_index < len(right):
+        if left[left_index] < right[right_index]:
+            result.append(left[left_index])
+            left_index += 1
+        else:
+            result.append(right[right_index])
+            right_index += 1
+    result += left[left_index:]
+    result += right[right_index:]
+    return result
+
+print(merge_sort([5, 3, 7, 2, 8, 4, 1, 9, 6]))
+
+
