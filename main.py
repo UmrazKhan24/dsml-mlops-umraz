d07c224ec354038ad127674bcff5f4f1918bc661
UmrazKhan24 <umrazkhan007@gmail.com>
2025-02-02 18:38:59 +0530

    adding insertion sort to v1

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
+
+# add insertion sort algorithm to sort list of numbers
+
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+    return arr
+
+print(insertion_sort([5, 3, 7, 2, 8, 4, 1, 9, 6]))
+
+
+
+
+
+
 # create quick sort algorithm for sorting list of numbers
 
 def quick_sort(arr):
